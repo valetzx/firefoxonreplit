8e75d5156c13a47ada6832c18a8476331849c297
valetzx <45054400+valetzx@users.noreply.github.com>
2022-07-29 19:08:26 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -12,6 +12,21 @@ echo $firefox
 echo $chrome
 
 echo Firefox start !
+#before all copy cookie
+#cp -r ./.mozilla ~/
 #chromedriver --no-sandbox > chrome.md &
 #chromium --no-sandbox
-firefox --new-tab github.com/valetzx --no-sandbox
+firefox -new-tab github.com/valetzx --no-sandbox &
+#以下配置为xdotool保活，需启用请取消相应注释
+#休眠sleep时间需自行配置
+sleep 60s
+echo Codespace Done !
+sh xdotool.sh
+sleep 30s
+xdotool sleep 30s
+sh xdofocus.sh
+sleep 30s
+xdotool search --name "space" key "Shift+T"
+sh xdoauto.sh
+sleep 12h
+kill 1
